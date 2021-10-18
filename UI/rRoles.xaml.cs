b90fcbffeef1a7c49feb2e48ba7a4802eddd275d
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-10-18 19:09:03 -0400

    Listo

--- UI/rRoles.xaml.cs
+++ UI/rRoles.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using RegistroDetalleAct.Entidades;
+using RegistroDetalleAct.BLL;
 
 
 namespace RegistroDetalleAct.UI
@@ -27,26 +28,131 @@ namespace RegistroDetalleAct.UI
         public rRoles()
         {
             InitializeComponent();
+
+            this.DataContext = rol;
+
+            PermisosComboBox.ItemsSource = PermisoBLL.GetPermisos();
+
+            PermisosComboBox.SelectedValuePath = "PermisoId";
+
+            PermisosComboBox.DisplayMemberPath = "Nombre";
+        }
+        private void Cargar()
+        {
+            this.DataContext = null;
+
+            this.DataContext = rol;
+        }
+        private void Limpiar()
+        {
+            this.rol = new Roles();
+
+            this.DataContext = rol;
+        }
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (PermisosComboBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Ha ocurrido un error, Ingrese el Permiso", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            return esValido;
+        }
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Roles esValido = RolesBLL.Buscar(rol.RolId);
+
+            return (esValido != null);
+        }
+
+        private bool ValidarGuardar()
+        {
+            bool esValido = true;
+
+            if (DetalleDataGrid.Items.Count == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Ha ocurrido un error, Debe agregar roles", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            return esValido;
         }
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+            Roles encontrado = RolesBLL.Buscar(rol.RolId);
 
+            if (encontrado != null)
+            {
+                rol = encontrado;
+
+                Cargar();
+            }
+            else
+            {
+                Limpiar();
+
+                MessageBox.Show("El Rol no existe en la base de datos", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void NuevoButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Limpiar();
         }
 
         private void GuardarButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!ValidarGuardar())
+                return;
 
+            bool paso = false;
+
+            if (rol.RolId == 0)
+            {
+                paso = RolesBLL.Guardar(rol);
+            }
+            else
+            {
+                if (ExisteEnLaBaseDeDatos())
+                {
+                    paso = RolesBLL.Guardar(rol);
+                }
+                else
+                {
+                    MessageBox.Show("No existe en la base de datos", "ERROR");
+                }
+            }
+
+            if (paso)
+            {
+                Limpiar();
+
+                MessageBox.Show("Guardado!", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Ocurrio un Error al guardar", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
         {
+            Roles existe = RolesBLL.Buscar(rol.RolId);
 
+            if (existe == null)
+            {
+                MessageBox.Show("No existe la tarea en la base de datos", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                return;
+            }
+            else
+            {
+                RolesBLL.Eliminar(rol.RolId);
+
+                MessageBox.Show("Eliminado!", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                Limpiar();
+            }
         }
     }
 }
