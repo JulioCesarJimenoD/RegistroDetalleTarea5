bf1220438ad82f62504f509cddcc675edbbdb449
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-10-18 18:41:40 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RegistroDetalleAct.Entidades;
+
+
+
+namespace RegistroDetalleAct.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Permisos> Permisos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlite(@"Data Source = DATA\RegistroDatalleAct.db");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 1,
+                Nombre = "Permiso",
+                Descripcion = "Permiso otorgado"
+            });
+
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 2,
+                Nombre = "Permiso de Emergencia",
+                Descripcion = "Permiso otorgado por inconveniente"
+            });
+
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 3,
+                Nombre = "Permiso de Salud",
+                Descripcion = "Permiso otorgado para licencia de salud"
+            });
+
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 4,
+                Nombre = "Permiso por Fallecimiento",
+                Descripcion = "Permiso otorgado por muerte de un familiar"
+            });
+        }
+    }
+}
---
+++ Entidades/RolesDetalle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDetalleAct.Entidades
+{
+   public class RolesDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int RolId { get; set; }
+        public int PermisoId { get; set; }
+        public bool  esValido { get; set; }
+
+        public RolesDetalle()
+        {
+            Id = 0;
+            RolId = 0;
+            PermisoId = 0;
+            esValido = false;
+        }
+
+        public RolesDetalle(int rolId, int permisoId, bool asignado)
+        {
+            Id = 0;
+            RolId = 0;
+            PermisoId = 0;
+            esValido = asignado;
+        }
+    }
+}
